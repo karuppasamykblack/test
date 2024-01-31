f6f62e113e7af0e97bc22f29b604637a48379984
karuppasamy.k <karuppasamy>
2024-01-31 17:18:23 +0530

    init commit

---
+++ mylibrarygit/src/main/java/com/example/mylibrarygit/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.mylibrarygit;
+
+import android.util.Log;
+
+public class GitTest {
+    public static void main(String[]args){
+
+    }
+    public static void printLog(String s){
+        Log.d("Key-->",s);
+    }
+}
