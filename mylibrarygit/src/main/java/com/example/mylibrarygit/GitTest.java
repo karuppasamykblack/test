772e38a02afea62c0c538d0fdadaf9b0d4e56dba
karuppasamy.k <karuppasamy>
2024-02-02 10:44:29 +0530

    add methods

--- mylibrarygit/src/main/java/com/example/mylibrarygit/GitTest.java
+++ mylibrarygit/src/main/java/com/example/mylibrarygit/GitTest.java
@@ -1,12 +1,29 @@
 package com.example.mylibrarygit;
 
+import android.app.Application;
 import android.util.Log;
+import android.widget.Toast;
 
-public class GitTest {
-    public static void main(String[]args){
+public class GitTest extends Application {
+    private static GitTest instance;
+
+    public static GitTest getInstance() {
+        if (instance == null) {
+            instance = new GitTest();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
 
     }
-    public static void printLog(String s){
-        Log.d("Key-->",s);
+
+    public static void printLog(String s) {
+        Log.d("Key-->", s);
     }
+
+    public void printToast(String s) {
+        Toast.makeText(getApplicationContext(), "HI", Toast.LENGTH_SHORT).show();
+    }
+
 }
